52e4181e944841a86f93f5c34e2ba4b1f89ce140
codenapps <team@codenapps.com>
2026-06-03 11:12:58 -0700

    new changes

--- config/bullmq.js
+++ config/bullmq.js
@@ -1,7 +1,6 @@
 const connection = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 };
 
 module.exports = { connection };
--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,8 @@
-const { Redis } = require("ioredis");
+const { Redis } = require("upstash/redis");
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null,
+  UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 module.exports = redis;
