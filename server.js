2847ae52eeaf36f8a82c405c7cb7a51efb15c532
codenapps <team@codenapps.com>
2026-06-04 11:28:25 -0700

    server update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-
+const productRouter = require("./routes/product.route");
 const connectDB = require("./config/db");
 connectDB().catch((err) => console.error("Mongo DB connection error", err));
 
@@ -8,3 +8,4 @@ require("./workers/product.worker");
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+app.use("/api/products", productRouter);
